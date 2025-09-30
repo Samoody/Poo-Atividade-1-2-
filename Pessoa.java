469b397355eded58c7774b4721233dbaf33e5cb7
Samuel Silva <54758700+Samoody@users.noreply.github.com>
2025-09-30 12:27:14 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,34 @@
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    // Construtor
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    // Método para fazer aniversário
+    public void fazerAniversario() {
+        this.idade++;
+    }
+}
